63d2e0f47fdcbdda76a4c00d8df62981806e2b63
Rafael <rafaelluna10@hotmail.com>
2025-05-27 21:01:45 -0300

    alterando superclasse

--- src/Usuario.java
+++ src/Usuario.java
@@ -4,12 +4,10 @@ import java.util.List;
 public class Usuario {
     private String nome;
     private int id;
-    private List<Emprestimo> emprestimoUsuario;
 
     public Usuario(String nome, int id) {
         this.nome = nome;
         this.id = id;
-        this.emprestimoUsuario = new ArrayList<>();
     }
 
     public String getNome() {
@@ -26,20 +24,6 @@ public class Usuario {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public List<Emprestimo> getEmprestimoUsuario() {
-        return emprestimoUsuario;
-    }
-
-    public void adicionarEmprestimo(Emprestimo emprestimo) {
-        if (emprestimo != null && !this.emprestimosDoUsuario.contains(emprestimo)) {
-            this.emprestimosDoUsuario.add(emprestimo);
-        }
-    }
-
-    public void removerEmprestimo(Emprestimo emprestimo) {
-        this.emprestimosDoUsuario.remove(emprestimo);
     }
 
     @Override
