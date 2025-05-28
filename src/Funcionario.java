9d88f41964eca3ac8b9416d994b620f0d9368640
Rafael <rafaelluna10@hotmail.com>
2025-05-27 21:07:02 -0300

    adicionando func

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,3 +1,36 @@
-public class Funcionario extends Usuario {
 
+public class Funcionario extends Usuario {
+    private String cargo;
+    private String setor;
+
+    public Funcionario(String nome, int id, String cargo, String setor) {
+        super(nome, id);
+        this.cargo = cargo;
+        this.setor = setor;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{{" +
+                "nome='" + getNome() + '\'' +
+                "cargo='" + cargo + '\'' +
+                ", setor='" + setor + '\'' +
+                '}';
+    }
 }
